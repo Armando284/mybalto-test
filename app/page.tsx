caf1df0a75c8b85534db382f5c2dd2920b045f23
Armando <arman2p284@gmail.com>
2025-07-20 13:46:13 -0400

    refactor: adds suspense for home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import PatientCharts from './ui/patientsCharts'
 import PatientsTable from './ui/clientPatientsTable'
 import { fetchPatients } from './lib/api/client'
 import { type PatientsResponse } from './lib/definitions'
+import { Suspense } from 'react'
 
 export default async function Page() {
 	const data: PatientsResponse = await fetchPatients()
@@ -9,15 +10,19 @@ export default async function Page() {
 	return (
 		<main className="w-full min-h-screen bg-slate-100 text-gray-800">
 			{/* Sort / Organize / Group pet data - Show in some simple display */}
-			<div className="container mx-auto py-8">
-				<h1 className="text-2xl font-bold mb-6">Patients</h1>
-				<PatientsTable initialData={data} />
-			</div>
+			<Suspense fallback={<div>Loading...</div>}>
+				<div className="container mx-auto py-8">
+					<h1 className="text-2xl font-bold mb-6">Patients</h1>
+					<PatientsTable initialData={data} />
+				</div>
+			</Suspense>
 			{/* Show data in a graph / chart */}
-			<div className="container mx-auto py-8">
-				<h1 className="text-2xl font-bold mb-6">Patient Charts</h1>
-				<PatientCharts initialData={data} />
-			</div>
+			<Suspense fallback={<div>Loading charts...</div>}>
+				<div className="container mx-auto py-8">
+					<h1 className="text-2xl font-bold mb-6">Patient Charts</h1>
+					<PatientCharts initialData={data} />
+				</div>
+			</Suspense>
 		</main>
 	)
 }
